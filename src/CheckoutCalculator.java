82c4556b0c112834b7bafcf4853327f37dc54681
ymca85 <bhushan.ymca85@gmail.com>
2016-12-01 23:27:57 +0530

    green - added logic to test for items A and B checkout

--- src/CheckoutCalculator.java
+++ src/CheckoutCalculator.java
@@ -4,18 +4,23 @@ import java.util.List;
 public class CheckoutCalculator {
 
 	public int calculateTotalAmountAtCheckout(List<String> listOfitemCode) {
-		
-		return getPriceOfItem(listOfitemCode.get(0));
+
+		int totalPriceOfAllItems = 0;
+		for (String itemCode : listOfitemCode) {
+			totalPriceOfAllItems += getPriceOfItem(itemCode);
+
+		}
+		return totalPriceOfAllItems;
 	}
 
 	private int getPriceOfItem(String itemCode) {
-		
+
 		HashMap<String, Integer> itemPriceMapper = new HashMap<String, Integer>();
-		
+
 		itemPriceMapper.put("A", 100);
 		itemPriceMapper.put("B", 200);
 		itemPriceMapper.put("C", 300);
-		
+
 		return itemPriceMapper.getOrDefault(itemCode, 0);
 	}
 
