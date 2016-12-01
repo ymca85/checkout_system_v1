35ece53513c100b5970cf956668c1694bd255671
ymca85 <bhushan.ymca85@gmail.com>
2016-12-02 00:27:22 +0530

    green - created logic to update total price for discounted price of item A

--- src/CheckoutCalculator.java
+++ src/CheckoutCalculator.java
@@ -4,13 +4,13 @@ import java.util.List;
 public class CheckoutCalculator {
 
 	private HashMap<String, Integer> itemPriceMapper = new HashMap<String, Integer>();
-	
+
 	public CheckoutCalculator() {
 		itemPriceMapper.put("A", 100);
 		itemPriceMapper.put("B", 200);
 		itemPriceMapper.put("C", 300);
 	}
-	
+
 	public int calculateTotalAmountAtCheckout(List<String> listOfitemCode) {
 
 		int totalPriceOfAllItems = 0;
@@ -18,6 +18,18 @@ public class CheckoutCalculator {
 			totalPriceOfAllItems += getPriceOfItem(itemCode);
 
 		}
+
+		int countOfItemA = 0;
+		for (String itemCode : listOfitemCode) {
+			if (itemCode.equals("A")) {
+				countOfItemA++;
+			}
+		}
+		if (countOfItemA >= 2) {
+			totalPriceOfAllItems = totalPriceOfAllItems + (90 * countOfItemA)
+					- (getPriceOfItem("A") * countOfItemA);
+		}
+
 		return totalPriceOfAllItems;
 	}
 
