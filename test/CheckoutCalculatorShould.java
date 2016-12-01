dd246bf8d91e49df3f33dd34e8dc23f881740d0d
ymca85 <bhushan.ymca85@gmail.com>
2016-12-01 22:43:57 +0530

    red-added test for checkout of item B

--- test/CheckoutCalculatorShould.java
+++ test/CheckoutCalculatorShould.java
@@ -11,5 +11,13 @@ public class CheckoutCalculatorShould {
 		assertEquals(100, checkoutCalculator.calculateTotalAmountAtCheckout("A"));
 
 	}
+	
+	@Test
+	public void return_amount_200_for_item_B() {
+
+		CheckoutCalculator checkoutCalculator = new CheckoutCalculator();
+		assertEquals(200, checkoutCalculator.calculateTotalAmountAtCheckout("B"));
+
+	}
 
 }
